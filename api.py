88f1d934423a2fa71644792340b6d56f989ad77e
Divyam Singla <44860971+divyamsingla0405@users.noreply.github.com>
2021-09-21 01:10:01 +0530

    Update api.py

--- api.py
+++ api.py
@@ -28,9 +28,5 @@ def home():
 def predict():
         return render_template('predict.html' , x = x )
 
-def open_browser():
-      webbrowser.open_new('http://127.0.0.1:5000/')
-
 if __name__ == "__main__":
-      Timer(1, open_browser).start();
-      app.run(port=5000)
+      app.run()
