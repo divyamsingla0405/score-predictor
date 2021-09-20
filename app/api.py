ead16354b162a1b37f5df524c398bc0ee463e3ef
Divyam Singla <44860971+divyamsingla0405@users.noreply.github.com>
2021-09-21 01:43:12 +0530

    Add files via upload

---
+++ app/api.py
@@ -0,0 +1,31 @@
+import pandas as pd
+# from werkzeug.wrappers import Request, Response
+from flask import Flask, render_template, request, jsonify, redirect
+import pickle
+import webbrowser
+import numpy as np
+
+# load model
+model = pickle.load(open('D:\Chola Finance\divyam file\model.pkl','rb'))
+
+# app
+app = Flask(__name__)
+
+x=[]
+# routes
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        hours = request.form.get('hours')       
+        prediction = model.predict([[np.array(hours)]])
+        # Take the first value of prediction
+        x.append(prediction)
+        return redirect ('/predict')
+    return render_template('home.html')
+
+@app.route('/predict')
+def predict():
+        return render_template('predict.html' , x = x )
+
+if __name__ == "__main__":
+        app.run()
